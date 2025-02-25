cca087c9f1b4fcbb9b6185fbeea166fb5fcbba46
tijodevasia <tijodevasia2003@gmail.com>
2025-02-25 11:36:19 +0530

    backend change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB =async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/foodDB').then(()=>console.log("DB Connected")
+    await mongoose.connect('mongodb+srv://tijodevasia:9567369450@tijo.p1zqx.mongodb.net/?retryWrites=true&w=majority&appName=tijo').then(()=>console.log("DB Connected")
     )
 }
