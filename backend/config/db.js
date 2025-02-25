3dc540937297966715a06ed4fe2706e4f78ed275
tijodevasia <tijodevasia2003@gmail.com>
2025-02-25 11:45:40 +0530

    Database_connect change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB =async ()=>{
-    await mongoose.connect('mongodb+srv://tijodevasia:9567369450@tijo.p1zqx.mongodb.net/?retryWrites=true&w=majority&appName=tijo').then(()=>console.log("DB Connected")
+    await mongoose.connect(process.env.DATABASE_URL).then(()=>console.log("DB Connected")
     )
 }
